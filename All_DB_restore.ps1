2e6febb357b0dad9671d7a6e2405498afdd0de0f
aaronlewispdx <aarnlewis@gmail.com>
2018-05-11 09:28:02 -0700

    Add files via upload

---
+++ All_DB_restore.ps1
@@ -0,0 +1,6 @@
+﻿Connect-Rubrik -Server '172.17.28.19' -Username 'aaron.lewis@rubrik.demo' -Password (ConvertTo-SecureString "Rubrik123!" -asplaintext -force) -Verbose
+
+Get-RubrikDatabase -Hostname se-alewis-win.rubrik.demo |
+   Where-Object {@('master','model','msdb') -notcontains $_.name -and $_.isRelic -ne 'True' -and $_.isLiveMount -ne 'TRUE'} |
+   Get-RubrikDatabase -Verbose |
+   ForEach-Object {Restore-RubrikDatabase -id $_.id -FinishRecovery -verbose }
